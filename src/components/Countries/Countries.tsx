947f815890394528dc716acbf0d3ec6da69e0f3a
Rahat Karim <rahatkarim5452@gmail.com>
2026-09-02 21:23:54 +0600

    Countries Country

---
+++ src/components/Countries/Countries.tsx
@@ -0,0 +1,22 @@
+import { use } from "react";
+import type CountryType from "../../type";
+import Country from "../Country/Country";
+
+export interface CountriesProps {
+  countriesPromise: Promise<CountryType[]>;
+}
+
+export default function Countries({ countriesPromise }: CountriesProps) {
+  const countries = use(countriesPromise);
+  console.log(countries);
+  return (
+    <div>
+      <h2>Countries: </h2>
+      <ol>
+        {countries.map((country) => (
+          <Country key={country.ccn3.ccn3} country={country}></Country>
+        ))}
+      </ol>
+    </div>
+  );
+}
